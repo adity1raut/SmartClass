96e1b81fbf2e96b237055d975a03e27494d9f571
Aditya Raut <araut7798@gmail.com>
2026-04-03 09:19:17 +0530

    Solve the Ci
    Signed-off-by: Aditya Raut <araut7798@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const backendUrl = env.BACKEND_URL || 'http://localhost:5000'
+  const env = loadEnv(mode, import.meta.url, '')
+  const backendUrl = env.VITE_BACKEND_URL || 'http://localhost:5000'
 
   return {
     plugins: [react(), tailwindcss()],
